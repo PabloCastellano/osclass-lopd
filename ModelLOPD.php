7f2521ebf34635a418488c52ba8fbe47a5c1d18e
Pablo Castellano <pablo@anche.no>
2013-08-06 23:29:34 +0200

    Version 0.9 (2012-02-15)
    http://forums.osclass.org/espanol-8/ley-proteccion-datos-lopd/msg22203/#msg22203

--- ModelLOPD.php
+++ ModelLOPD.php
@@ -52,7 +52,7 @@
             parent::__construct();
             $this->setTableName('t_lopd') ;
             $this->setPrimaryKey('fk_i_user_id') ;
-            $this->setFields( array('fk_i_user_id', 'dt_date', 's_ip') ) ;
+            $this->setFields( array('fk_i_user_id', 'dt_date', 's_ip', 'b_could_delete') ) ;
         }
         
         /**
@@ -83,6 +83,80 @@
             $this->dao->query(sprintf('DROP TABLE %s', $this->getTable()) ) ;
         }
         
+        public function hasAccepted($userId) {
+            $this->dao->select('*') ;
+            $this->dao->from($this->getTable()) ;
+            $this->dao->where('fk_i_user_id', $userId);
+            $result = $this->dao->get() ;
+
+            if( $result->numRows == 0 ) {
+                return false ;
+            }
+
+            return $result->row()>0?true:false;
+        }
+        
+        public function acceptLOPD($userId) {
+            ModelLOPD::newInstance()->insert(array(
+                'fk_i_user_id' => $userId,
+                'dt_date' => date('Y-m-d H:i:s'),
+                's_ip' => @$_SERVER['REMOTE_ADDR']
+            ));
+        }
+        
+        public function couldDelete($userId) {
+            $this->dao->select('*') ;
+            $this->dao->from($this->getTable()) ;
+            $this->dao->where('fk_i_user_id', $userId);
+            $result = $this->dao->get() ;
+
+            if( $result->numRows == 0 ) {
+                return false ;
+            }
+
+            $row = $result->row();
+            return $row['b_could_delete']==1?true:false;
+        }
+
+/**
+         * Return list of users
+         * 
+         * @access public
+         * @since unknown
+         * @param int $start
+         * @param int $end
+         * @param string $order_column
+         * @param string $order_direction
+         * @return array
+         */
+        public function search($start = 0, $end = 10, $order_column = 'pk_i_id', $order_direction = 'DESC')
+        {
+            
+            // SET data, so we always return a valid object
+            $users = array();
+            $users['rows'] = 0;
+            $users['total_results'] = 0;
+            $users['users'] = array();
+            
+            $sql = sprintf("SELECT SQL_CALC_FOUND_ROWS u.* FROM %st_user u, %st_lopd l WHERE u.pk_i_id = l.fk_i_user_id ORDER BY %s %s LIMIT %s, %s", DB_TABLE_PREFIX, DB_TABLE_PREFIX, $order_column, $order_direction, $start, $end);
+            $result = $this->dao->query($sql) ;
+            
+            if(!$result) {
+                return $users;
+            }
+            
+            $datatmp  = $this->dao->query('SELECT FOUND_ROWS() as total');
+            $data = $datatmp->row();
+            if(isset($data['total'])) {
+                $users['total_results'] = $data['total'];
+            }
+            
+            $users['users'] = $result->result();
+            $users['rows'] = $result->numRows();
+            
+            
+            return $users;
+        }        
     }
 
 ?>
--- user_menu.php
+++ user_menu.php
@@ -1,6 +1,6 @@
 <?php
     $user = User::newInstance()->findByPrimaryKey(osc_logged_user_id());
-    if(Params::getParam('confirm')=='true' && Params::getParam('s')==$user['s_secret']) {
+    if(Params::getParam('confirm')=='true' && Params::getParam('s')==$user['s_secret'] && ModelLOPD::newInstance()->couldDelete(osc_logged_user_id())) {
         Log::newInstance()->insertLog('user', 'delete', osc_logged_user_id(), $user['s_email'], 'LOPD Plugin', osc_logged_user_id());
         User::newInstance()->deleteUser(osc_logged_user_id());
         Session::newInstance()->_drop('userId') ;
@@ -8,9 +8,9 @@
         Session::newInstance()->_drop('userEmail') ;
         Session::newInstance()->_drop('userPhone') ;
 
-        Cookie::newInstance()->pop('oc_userId') ;
-        Cookie::newInstance()->pop('oc_userSecret') ;
-        Cookie::newInstance()->set() ;
+        @Cookie::newInstance()->pop('oc_userId') ;
+        @Cookie::newInstance()->pop('oc_userSecret') ;
+        @Cookie::newInstance()->set();
 ?>
         <script type="text/javascript">
             window.location = "<?php echo osc_base_url(); ?>"
@@ -29,9 +29,14 @@
                     <?php echo osc_private_user_menu() ; ?>
                 </div>
                 <div id="main">
-                    <h2><?php _e('Borrar centa', 'paypal'); ?></h2>
+                    <h2><?php _e('Borrar centa', 'lopd'); ?></h2>
+                    <?php if(ModelLOPD::newInstantece()->couldDelete(osc_logged_user_id())) { ?>
                         <h3><?php _e('Está a punto de borrar su cuenta, borrará su cuenta de usuario y sus anuncios publicados ¿Desea continuar?', 'lopd'); ?></h3>
                         <a href="<?php echo osc_render_file_url(osc_plugin_folder(__FILE__)."user_menu.php")."&confirm=true&s=".$user['s_secret']; ?>" /><?php _e('Entiendo los riesgos y desde BORRAR MI CUENTA', 'lopd'); ?></a>
+                    <?php } else { ?>
+                        <h3><?php _e('En estos momentos no puede borrar su cuenta.', 'lopd'); ?></h3>
+                        <p><?php echo sprintf(__('Posiblemente tenga algún pago o algún otro asunto pendiente en la web y su cuenta no puede ser borrada en estos momentos. Si aún así desea borrar su cuenta, por favor <a href="">contacte con nosotros</a>.', 'lopd'), osc_contact_url()); ?></p>
+                    <?php }; ?>
                 </div>
             </div>
 
